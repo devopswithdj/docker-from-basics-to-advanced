d22d69b4665913e134ca05ad573038f7ebee611b
devopswithdj <bdhanunjayardy@gmail.com>
2025-10-17 07:00:19 +0530

    Updating Files

--- Day-02/portfolio-website/backend/processors/userProcessor.js
+++
@@ -1,6 +0,0 @@
-import { userData } from "../models/userModel.js";
-
-export const getUserData = () => {
-  // Additional business logic or data formatting can go here
-  return userData;
-};
--- Day-02/portfolio-website/backend/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getProfile } from "../controllers/userController.js";
-
-const router = express.Router();
-router.get("/profile", getProfile);
-
-export default router;
