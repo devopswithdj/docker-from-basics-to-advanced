fbe770aedc4b0f1f2817dd4afee7faf50b662161
devopswithdj <bdhanunjayardy@gmail.com>
2025-10-17 07:00:42 +0530

    Removing node modules

---
+++ Day-02/portfolio-website/backend/models/userModel.js
@@ -0,0 +1,19 @@
+// Dummy data model (can be MongoDB schema later)
+export const userData = {
+  name: "Dhanunjaya",
+  role: "DevOps Engineer",
+  about: "Passionate about building scalable web applications and working with cutting-edge technologies.",
+  skills: ["React", "Node.js", "Material UI", "SQL", "Docker"],
+  projects: [
+    {
+      title: "Complete DevSecOps End to End",
+      description: "Tic-Tac-Toe game implementing React, Node, Testing, Docker, CI/CD, Argocd, K8s",
+      link: "https://github.com/devopswithdj/devsecops-demo.git"
+    },
+    {
+      title: "Terraform Zero to Hero",
+      description: "A Learning oriented repository to learn Terraform from scratch to advanced concepts.",
+      link: "https://github.com/devopswithdj/terraform-zero-to-hero.git"
+    }
+  ]
+};
